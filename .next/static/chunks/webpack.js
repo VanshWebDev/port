dc404efea05c4a482258f16aba4ddbff97ffab62
VanshWebDev <vanshvanshkumar39@gmail.com>
2024-08-16 06:42:40 +0530

    updated resume

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f98097d665361434"; }
+/******/ 		__webpack_require__.h = function() { return "7118df312de4fec7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/_app.adff69b61d494633.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./content/footer.json":
+/*!*****************************!*\
+  !*** ./content/footer.json ***!
+  \*****************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"acknowledgments":[{"person":"Full stack development certificate","link":"/img/certificate.pdf","note":"Delta 3.0."},{"person":"LeetCode","link":"https://leetcode.com/u/DevVanshKumar/","note":"Checkout our leetcode profile."},{"person":"HackerRank","link":"https://www.hackerrank.com/devloper.mind_","note":"Checkout our hackerrank profile ."}],"links":[{"person":"Mail us on","link":"mailto: vanshvanshkumar39@gmail.com","note":"vanshvanshkumar39@gmail.com."}],"social":[{"url":"https://www.linkedin.com/in/vansh-kumar-webdev/","icon":"linkedin"},{"url":"https://github.com/VanshWebDev","icon":"github"},{"url":"https://instagram.com/devloper.mind_","icon":"instagram"}]}');
+
+/***/ })
+
+});
